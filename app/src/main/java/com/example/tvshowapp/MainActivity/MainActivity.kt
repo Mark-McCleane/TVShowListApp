c4cbb246c6df4bf6cbaf104042b26ec738545537
Mark-McCleane <mccleanemark@gmail.com>
2023-03-12 23:10:11 +0000

    implemented basic functionality

--- app/src/main/java/com/example/tvshowapp/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/tvshowapp/MainActivity/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.tvshowapp.MainActivity
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +10,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tvshowapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -21,13 +25,23 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     private var page: Int = 1
     private lateinit var mTvShowViewModel: TvShowViewModel
+    private var sortingSharedPref: SharedPreferences? = null
     private var sortedAsc = true
+    private lateinit var floatingActionButton: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var retrofitApi = Retrofit.Builder()
+        sortingSharedPref = getSharedPreferences(SORTINGSHAREPREF, Context.MODE_PRIVATE)
+
+        if (sortingSharedPref != null && sortedAsc != sortingSharedPref!!.getBoolean(SORTING,
+                false)
+        ) {
+            sortedAsc = sortingSharedPref!!.getBoolean(SORTING, false)
+        }
+
+        val retrofitApi = Retrofit.Builder()
             .baseUrl(Companion.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
@@ -37,7 +51,19 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view_tvShowList)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
+
         mTvShowViewModel = ViewModelProvider(this).get(TvShowViewModel::class.java)
+        floatingActionButton = findViewById(R.id.floatingActionButton)
+        floatingActionButton.setOnClickListener {
+            sortedAsc = !sortedAsc
+            val editor = sortingSharedPref!!.edit()
+            editor.putBoolean(SORTING, sortedAsc)
+            editor.apply()
+            val restartIntent: Intent = intent
+            finish()
+            startActivity(restartIntent)
+        }
+
         if (sortedAsc) {
             mTvShowViewModel.getTvShowListAsc()
                 .observe(this, androidx.lifecycle.Observer { newData ->
@@ -67,5 +93,7 @@ class MainActivity : AppCompatActivity() {
             "25a8f80ba018b52efb64f05140f6b43c" //todo move to local properties for best practice
         private const val BASE_URL: String = "https://api.themoviedb.org"
         private const val LANGUAGE: String = "en-US"
+        private const val SORTINGSHAREPREF: String = "sorting_share_pref"
+        private const val SORTING: String = "sorting"
     }
 }
