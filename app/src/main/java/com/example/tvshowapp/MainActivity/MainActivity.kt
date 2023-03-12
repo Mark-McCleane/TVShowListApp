b6424eae096d6c4fa2b57146b5fa17e8a6f7066c
Mark-McCleane <mccleanemark@gmail.com>
2023-03-12 01:14:33 +0000

    connected to api

--- app/src/main/java/com/example/tvshowapp/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/tvshowapp/MainActivity/MainActivity.kt
@@ -1,15 +1,55 @@
 package com.example.tvshowapp.MainActivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.tvshowapp.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.await
+import retrofit2.converter.gson.GsonConverterFactory
+import utils.RetrofitService
 
 class MainActivity : AppCompatActivity() {
 
 
+    private val TAG = "MainActivity"
+    private var page: Int = 1
+    private lateinit var textView: TextView
+    private lateinit var arrayList: ArrayList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var retrofitApi = Retrofit.Builder()
+            .baseUrl(Companion.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RetrofitService::class.java)
+        textView = findViewById(R.id.sampleTextView)
+        arrayList = ArrayList();
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val responses = retrofitApi.getAllTvShows(API_KEY, LANGUAGE, page).await()
+
+            Log.e(TAG, "Page = ${responses.page}")
+            for (result in responses.results) {
+                arrayList.add(result.name)
+            }
+
+            Log.e(TAG, arrayList[0]); //PROOF DATA IS PULLLING FROM API
+        }
+
         //https://api.themoviedb.org/3/tv/top_rated?api_key=25a8f80ba018b52efb64f05140f6b43c&language=en-US&page=1
+    }
+
+    companion object {
+        private const val API_KEY: String = "25a8f80ba018b52efb64f05140f6b43c"
+        private const val BASE_URL: String = "https://api.themoviedb.org"
+        private const val LANGUAGE: String = "en-US"
     }
 }
--- app/src/main/java/utils/RetrofitService.kt
+++ app/src/main/java/utils/RetrofitService.kt
@@ -1,11 +1,17 @@
 package utils
 
 import model.TVShowListPage
-import retrofit2.Response
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface RetrofitService {
 
-    @GET("3/tv/top_rated?api_key=25a8f80ba018b52efb64f05140f6b43c&language=en-US&page=1")
-    suspend fun getAllTvShows():Response<List<TVShowListPage>>
+    @GET("/3/tv/top_rated")
+    fun getAllTvShows(
+        @Query("api_key") apiKey:String,
+        @Query("language") language:String,
+        @Query("page") page:Int
+    ): Call<TVShowListPage>
 }
+//    https://api.themoviedb.org/3/tv/top_rated?api_key=25a8f80ba018b52efb64f05140f6b43c&language=en-US&page=1
