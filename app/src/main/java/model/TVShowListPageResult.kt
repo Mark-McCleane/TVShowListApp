0bf084647ba8b05c453cbbe1deedd6a32c642ae5
Mark-McCleane <mccleanemark@gmail.com>
2023-03-12 19:07:05 +0000

    implemented room into app

--- app/src/main/java/model/TVShowListPageResult.kt
+++ app/src/main/java/model/TVShowListPageResult.kt
@@ -8,10 +8,9 @@ import com.google.gson.annotations.SerializedName
 data class TVShowListPageResult(
     @SerializedName("backdrop_path") val backdropPath: String,
     @SerializedName("first_air_date") val firstAirDate: String,
-    @SerializedName("genre_ids") val genreIds: List<Int>,
     @PrimaryKey(autoGenerate = false) val id: Int,
     val name: String,
-    @SerializedName("origin_country") val originCountry: List<String>,
+//    @SerializedName("origin_country") val originCountry: List<String>, //todo figure out
     @SerializedName("original_language") val originalLanguage: String,
     @SerializedName("original_name") val originalName: String,
     val overview: String,
