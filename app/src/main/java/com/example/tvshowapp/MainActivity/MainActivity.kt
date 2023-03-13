bf0e984b96c53012413000d728e1e3467a8a465b
Mark-McCleane <mccleanemark@gmail.com>
2023-03-13 15:29:16 +0000

    moved coroutines to TvShowViewModel.kt

--- app/src/main/java/com/example/tvshowapp/MainActivity/MainActivity.kt
+++ app/src/main/java/com/example/tvshowapp/MainActivity/MainActivity.kt
@@ -4,26 +4,18 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tvshowapp.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.await
-import retrofit2.converter.gson.GsonConverterFactory
-import utils.RetrofitService
+import utils.TvShowConstants
 import utils.TvShowListAdapter
 import utils.TvShowViewModel
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
-    private var page: Int = 1
     private lateinit var mTvShowViewModel: TvShowViewModel
     private var sortingSharedPref: SharedPreferences? = null
     private var sortedAsc = true
@@ -33,31 +25,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        sortingSharedPref = getSharedPreferences(SORTINGSHAREPREF, Context.MODE_PRIVATE)
+        sortingSharedPref =
+            getSharedPreferences(TvShowConstants.SORTINGSHAREPREF, Context.MODE_PRIVATE)
+        mTvShowViewModel = ViewModelProvider(this).get(TvShowViewModel::class.java)
 
-        if (sortingSharedPref != null && sortedAsc != sortingSharedPref!!.getBoolean(SORTING,
-                false)
+
+        if (sortingSharedPref != null && sortedAsc !=
+            sortingSharedPref!!.getBoolean(TvShowConstants.SORTING, false)
         ) {
-            sortedAsc = sortingSharedPref!!.getBoolean(SORTING, false)
+            sortedAsc = sortingSharedPref!!.getBoolean(TvShowConstants.SORTING, false)
         }
-
-        val retrofitApi = Retrofit.Builder()
-            .baseUrl(Companion.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(RetrofitService::class.java)
 
         val adapter = TvShowListAdapter()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view_tvShowList)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        mTvShowViewModel = ViewModelProvider(this).get(TvShowViewModel::class.java)
         floatingActionButton = findViewById(R.id.floatingActionButton)
         floatingActionButton.setOnClickListener {
             sortedAsc = !sortedAsc
             val editor = sortingSharedPref!!.edit()
-            editor.putBoolean(SORTING, sortedAsc)
+            editor.putBoolean(TvShowConstants.SORTING, sortedAsc)
             editor.apply()
             val restartIntent: Intent = intent
             finish()
@@ -76,24 +64,8 @@ class MainActivity : AppCompatActivity() {
                 })
         }
 
-        GlobalScope.launch(Dispatchers.IO) {
-            val responses = retrofitApi.getAllTvShows(API_KEY, LANGUAGE, page).await()
-
-            for (result in responses.results) {
-                mTvShowViewModel.addTvShow(result)
-                Log.e(TAG, "Tv Show successfully added")
-            }
-        }
+        mTvShowViewModel.addTvShowsToRoomDb()
 
         //https://api.themoviedb.org/3/tv/top_rated?api_key=25a8f80ba018b52efb64f05140f6b43c&language=en-US&page=1
-    }
-
-    companion object {
-        private const val API_KEY: String =
-            "25a8f80ba018b52efb64f05140f6b43c" //todo move to local properties for best practice
-        private const val BASE_URL: String = "https://api.themoviedb.org"
-        private const val LANGUAGE: String = "en-US"
-        private const val SORTINGSHAREPREF: String = "sorting_share_pref"
-        private const val SORTING: String = "sorting"
     }
 }
--- app/src/main/java/utils/TvShowViewModel.kt
+++ app/src/main/java/utils/TvShowViewModel.kt
@@ -5,9 +5,13 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import model.TVShowListPageResult
 import model.TVShowListRepository
+import retrofit2.Retrofit
+import retrofit2.await
+import retrofit2.converter.gson.GsonConverterFactory
 
 class TvShowViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -35,5 +39,23 @@ class TvShowViewModel(application: Application) : AndroidViewModel(application) 
 
     fun getTvShowListAsc(): LiveData<List<TVShowListPageResult>> {
         return readAllDataAsc
+    }
+
+    fun addTvShowsToRoomDb() {
+        var page: Int = 1
+        val retrofitApi = Retrofit.Builder()
+            .baseUrl(TvShowConstants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RetrofitService::class.java)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val responses = retrofitApi.getAllTvShows(TvShowConstants.API_KEY,
+                TvShowConstants.LANGUAGE, page).await()
+
+            for (result in responses.results) {
+                addTvShow(result)
+            }
+        }
     }
 }
