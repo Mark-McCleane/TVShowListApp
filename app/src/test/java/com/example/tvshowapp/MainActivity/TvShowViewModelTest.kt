8482eb805b9f66218be4774cd9e3d676c85d7f9d
Mark-McCleane <mccleanemark@gmail.com>
2023-04-09 19:17:27 +0100

    added unit tests

--- app/src/test/java/com/example/tvshowapp/MainActivity/TvShowViewModelTest.kt
+++ app/src/test/java/com/example/tvshowapp/MainActivity/TvShowViewModelTest.kt
@@ -26,12 +26,56 @@ class TvShowViewModelTest {
     }
 
     @Test
-    fun `addTvShow item with empty fields returns error`() {
-        var tvShow: TVShow = TVShow("", "", 0,"","",
-            "","",0.0,"",0.0,0)
+    fun `addTvShow item with correct fields returns success`(){
+        val tvShow = TVShow("fads", "12/06/2021", 0, "Test","Test",
+            "Test","Test",0.0,"Test", 0.0, 0)
+        viewModel.addTvShow(tvShow)
+        val value = viewModel.insertTvShowStatus.getOrAwaitValueTest()
+        assert(value.getContentIfNotHandled()?.status == Status.SUCCESS)
+    }
+
+    @Test
+    fun `addTvShow item with incorrect name return error`(){
+        val tvShow = TVShow("fads", "12/06/2021", 0, "","Test",
+            "Test","Test",0.0,"Test", 0.0, 0)
         viewModel.addTvShow(tvShow)
         val value = viewModel.insertTvShowStatus.getOrAwaitValueTest()
         assert(value.getContentIfNotHandled()?.status == Status.ERROR)
     }
 
+    @Test
+    fun `addTvShow item with incorrect overview return error`(){
+        val tvShow = TVShow("fads", "12/06/2021", 0, "Test","Test",
+            "Test","",0.0,"Test", 0.0, 0)
+        viewModel.addTvShow(tvShow)
+        val value = viewModel.insertTvShowStatus.getOrAwaitValueTest()
+        assert(value.getContentIfNotHandled()?.status == Status.ERROR)
+    }
+
+    @Test
+    fun `addTvShow item with incorrect posterpath return success`(){
+        val tvShow = TVShow("fads", "12/06/2021", 0, "Test","Test",
+            "Test","Test",0.0,"", 0.0, 0)
+        viewModel.addTvShow(tvShow)
+        val value = viewModel.insertTvShowStatus.getOrAwaitValueTest()
+        assert(value.getContentIfNotHandled()?.status == Status.SUCCESS)
+    }
+
+    @Test
+    fun `addTvShow item with incorrect first air date return success`(){
+        val tvShow = TVShow("fads", "", 0, "Test","Test",
+            "Test","Test",0.0,"Test", 0.0, 0)
+        viewModel.addTvShow(tvShow)
+        val value = viewModel.insertTvShowStatus.getOrAwaitValueTest()
+        assert(value.getContentIfNotHandled()?.status == Status.SUCCESS)
+    }
+
+    @Test
+    fun `addTvShow item with empty fields returns error`() {
+        val tvShow: TVShow = TVShow("", "", 0,"","",
+            "","",0.0,"",0.0,0)
+        viewModel.addTvShow(tvShow)
+        val value = viewModel.insertTvShowStatus.getOrAwaitValueTest()
+        assert(value.getContentIfNotHandled()?.status == Status.ERROR)
+    }
 }
