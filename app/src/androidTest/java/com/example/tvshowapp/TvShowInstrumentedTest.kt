8a5f316f4879f554beaa20836cb75ceef896c7e5
Mark-McCleane <mccleanemark@gmail.com>
2023-04-09 19:25:55 +0100

    removed instrumented test

--- app/src/androidTest/java/com/example/tvshowapp/TvShowInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tvshowapp/TvShowInstrumentedTest.kt
@@ -16,27 +16,4 @@ import org.junit.runner.RunWith
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class TvShowInstrumentedTest {
-    private lateinit var testTvShow: TVShow
-    private lateinit var appContext: Context
-    private lateinit var mTvShowViewModel: TvShowViewModel
-
-    init {
-        appContext = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
-        mTvShowViewModel = TvShowViewModel(ApplicationProvider.getApplicationContext())
-        testTvShow = TVShow("Foo", "01,01,2022", 1, "Foo", "Foo",
-            "Foo", "Foo", 1000.00, "Foo", 5.9, 100)
-    }
-
-    @Test
-    fun Test1() {
-//        Create Mock
-        val tvShowsList = listOf<TVShow>(testTvShow)
-
-//        Call function using moc
-        mTvShowViewModel.addTvShow(testTvShow)
-
-//         check result
-        assertEquals(mTvShowViewModel.getTvShowListAsc(), tvShowsList)
-    }
-}
+class TvShowInstrumentedTest {}
